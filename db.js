d3585aae80495423547aa92224aab5da11349971
Enoka Jaona <Ejaona@gmail.com>
2019-03-26 20:00:07 -0500

    created db.js

---
+++ db.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const knex = require("knex");
+const dbEnv = process.env.DB_ENV || "development";
+const config = require("./knexfile")[dbEnv];
+
+const db = knex(config);
+
+module.exports = db;
